0b7ba02461f61da237a04325faf9030cc0da94f1
Kaitlyn OFlaherty <oflaherty.k@northeastern.edu>
2024-09-10 10:10:04 -0400

    WIP

--- posturebest/Views/ConfigurationsView.swift
+++ posturebest/Views/ConfigurationsView.swift
@@ -38,7 +38,7 @@ struct ConfigurationsView: View {
 //                    // PT stuff for good posture
 //                    // sync posture button
 //                    // timer and completion status
-//                                
+//
 //                    TextField("Enter a number", text: $userInput)
 //                        .keyboardType(.numberPad)
 //                        .textFieldStyle(RoundedBorderTextFieldStyle())
