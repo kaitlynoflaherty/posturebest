f33bd6e8b5b7ddd0784a5bc2ba2d9a48fc9106ed
Kaitlyn OFlaherty <oflaherty.k@northeastern.edu>
2024-09-11 11:54:26 -0400

    WIP

--- posturebest/ConfigurationsView.swift
+++ posturebest/ConfigurationsView.swift
@@ -9,31 +9,46 @@ import Foundation
 import SwiftUI
 
 struct ConfigurationsView: View {
-    @State private var selectedOption: String = "Vest Config" // Initial selected value
-       @State private var userInput: String = "" // User input
+    @State private var selectedOption: String = "Configure Device"
+    @State private var userInput: String = ""
        
-       let options = ["Vest Config", "Feedback", "Option 3"]
+    let options = ["Configure Device", "Configure Feedback"]
+    
     var body: some View {
         VStack {
             Text("Configurations")
                 .font(.largeTitle)
                 .padding()
-            // Dropdown menu (Picker)
+            
             Picker("Select an option", selection: $selectedOption) {
                 ForEach(options, id: \.self) { option in
                     Text(option).tag(option)
                 }
             }
-            .pickerStyle(SegmentedPickerStyle()) // You can use different styles like .wheel or .segmented
+            .pickerStyle(SegmentedPickerStyle())
             .padding()
             
-            Text("Vest Configuration")
-                        
-            // Number input (TextField)
-            TextField("Enter a number", text: $userInput)
-                .keyboardType(.numberPad) // This sets the keyboard to number pad
-                .textFieldStyle(RoundedBorderTextFieldStyle())
-                .padding()
+            if selectedOption == "Configure Device" {
+                // option for additional devices?
+                Text("Vest Configuration")
+                
+                // add instructions and context
+                // PT stuff for good posture
+                // sync posture button
+                // timer and completion status
+                            
+                TextField("Enter a number", text: $userInput)
+                    .keyboardType(.numberPad)
+                    .textFieldStyle(RoundedBorderTextFieldStyle())
+                    .padding()
+            } else {
+                Text("Feedback and Reminders")
+                            
+                TextField("Enter a number", text: $userInput)
+                    .keyboardType(.numberPad)
+                    .textFieldStyle(RoundedBorderTextFieldStyle())
+                    .padding()
+            }
                         
             Spacer()
         }
